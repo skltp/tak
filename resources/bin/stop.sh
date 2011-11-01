e1f32dc73c491f2a5204021b3a62bddb8ff5d96d
peterlarsson <peterlarsson@c1b456d0-7b02-511b-e5bf-04bacca435da>
2011-11-01 15:11:28 +0000

    Scripts finding home directory - more robust.

--- resources/bin/stop.sh
+++ resources/bin/stop.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-d=`dirname $0`/..
-export TK_HOME=`pwd $d`
-
+export TK_HOME="$( cd -P "$( dirname $0 )"/.. && pwd )"
 echo -n "Stopping Apache Tomcat 6.0.33..."
 cd ${TK_HOME}/apache-tomcat-6.0.33/bin
 ./shutdown.sh &>/dev/null
