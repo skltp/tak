c84ee975f78b5dd2c6f04d47a3177238054edc60
jonas.behmer@callistaenterprise.se <jonas.behmer@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2013-09-09 08:57:29 +0000

    Initierar databas i testläge med sql script

--- tp-vagval-admin-web/grails-app/conf/BootStrap.groovy
+++ tp-vagval-admin-web/grails-app/conf/BootStrap.groovy
@@ -18,62 +18,31 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-import se.skl.tp.vagval.admin.web.entity.*
+import groovy.sql.Sql
 
+
+import se.skl.tp.vagval.admin.web.entity.*
 class BootStrap {
 
+	def grailsApplication
      def init = { servletContext ->
         switch(grails.util.GrailsUtil.environment) {
         case "development":
         case "test":
-        	setupTestData()
-			setupUsers()
+			def config = grailsApplication.config
+			String initSqlFilePath = './grails-app/conf/testdata/init.sql'
+			String testDataSqlFilePath = './grails-app/conf/testdata/testdata.sql'
+			String initSql = new File(initSqlFilePath).text
+			String testDataSql = new File(testDataSqlFilePath).text
+			def sql = Sql.newInstance(config.dataSource.url,
+				config.dataSource.username,
+				config.dataSource.password,
+				config.dataSource.driverClassName)
+				sql.execute(initSql)
+				sql.execute(testDataSql)
 			break
         }
      }
      def destroy = {
-     }
-     
-     def setupTestData() {
-     	def rivVersion = new se.skl.tp.vagval.admin.core.entity.RivVersion()
-     	rivVersion.beskrivning = "Riv 1.0"
-		rivVersion.namn = "1.0"
-		rivVersion.save()
-		def kontrakt = new se.skl.tp.vagval.admin.core.entity.Tjanstekontrakt()
-     	kontrakt.namnrymd = "http://x.y.z"
-		kontrakt.beskrivning = "Tjänst XYZ"
-		kontrakt.save()
-		def tjansteKomponent = new se.skl.tp.vagval.admin.core.entity.Tjanstekomponent()
-     	tjansteKomponent.hsaId = "Komponent 1"
-     	tjansteKomponent.adress = "Address 1"
-     	tjansteKomponent.beskrivning = "Komponent 1"
-     	tjansteKomponent.save()
-		def logiskAdressat = new se.skl.tp.vagval.admin.core.entity.LogiskAdressat()
-     	logiskAdressat.hsaId = "Addressat 1"
-     	logiskAdressat.beskrivning = "Addressat 1"
-     	logiskAdressat.save()     	
-		def logiskAdress = new se.skl.tp.vagval.admin.core.entity.LogiskAdress()
-     	logiskAdress.fromTidpunkt = new java.sql.Date(System.currentTimeMillis())
-     	logiskAdress.tomTidpunkt = new java.sql.Date(System.currentTimeMillis())
-     	logiskAdress.rivVersion = rivVersion
-     	logiskAdress.tjanstekontrakt = kontrakt
-     	logiskAdress.logiskAdressat = logiskAdressat
-     	logiskAdress.tjansteproducent = tjansteKomponent
-     	logiskAdress.save()     	
-		def anropsbehorighet = new se.skl.tp.vagval.admin.core.entity.Anropsbehorighet()
-     	anropsbehorighet.fromTidpunkt = new java.sql.Date(System.currentTimeMillis())
-     	anropsbehorighet.tomTidpunkt = new java.sql.Date(System.currentTimeMillis())
-     	anropsbehorighet.integrationsavtal = "Avtal 1"
-     	anropsbehorighet.tjanstekontrakt = kontrakt
-     	anropsbehorighet.logiskAdressat = logiskAdressat
-     	anropsbehorighet.tjanstekonsument = tjansteKomponent
-     	anropsbehorighet.save()     	
-     }
-     
-     def setupUsers() {
-    	 // Administrators
-    	 def adminUser = new Anvandare(anvandarnamn: "admin", losenord: "admin", administrator: true).save()
-		 //	Users.
-		 def normalUser = new Anvandare(anvandarnamn: "bbe", losenord: "bbe", administrator: false).save()
      }
 }
