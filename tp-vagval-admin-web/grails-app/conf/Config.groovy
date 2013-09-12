f5261504d170489e12db5ed75ea3ed9b2e713862
jonas.behmer@callistaenterprise.se <jonas.behmer@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2013-09-12 06:03:07 +0000

    Lagt till konfiguration for resources och jquery

--- tp-vagval-admin-web/grails-app/conf/Config.groovy
+++ tp-vagval-admin-web/grails-app/conf/Config.groovy
@@ -50,6 +50,9 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
 // URL Mapping Cache Max Size, defaults to 5000
 //grails.urlmapping.cache.maxsize = 1000
 
+// What URL patterns should be processed by the resources plugin
+grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
+
 // The default codec used to encode data with ${}
 grails.views.default.codec = "none" // none, html, base64
 grails.views.gsp.encoding = "UTF-8"
