bd80a41e890cbffb391a68fecb53caebae93f17a
Hans Thunberg <hans.thunberg@callistaenterprise.se>
2014-11-21 11:44:20 +0100

    AV-22: Uppdatera TAK datamodel, script för att migrera och validera uppdaterade

--- resources/sql/migrera_till_142/2-migrate-data.sql
+++ resources/sql/migrera_till_142/2-migrate-data.sql
@@ -2,15 +2,28 @@
 # Migrera data
 #############################################################
 
+#####################
+# Anvandare
+#####################
+
 # Namnbyte på tabell anvandare till Anvandare, se Jira SKLTP-322
-INSERT INTO takv2.Anvandare(id, version, anvandarnamn, losenord_hash, administrator) \
-SELECT id, version, anvandarnamn, losenord_hash, administrator \
+INSERT INTO takv2.Anvandare(id, version, anvandarnamn, losenord_hash, administrator)
+SELECT id, version, anvandarnamn, losenord_hash, administrator
 FROM tp_admin.anvandare;
 
+#####################
+# RivTaProfil
+#####################
+
 # Namnbyte på tabell RivVersion till RivTaProfil
-INSERT INTO takv2.RivTaProfil(id, version, namn, beskrivning) \
-SELECT id, version, namn, beskrivning \
+INSERT INTO takv2.RivTaProfil(id, version, namn, beskrivning)
+SELECT id, version, namn, beskrivning
 FROM tp_admin.RivVersion;
+
+
+#####################
+# Tjanstekontrakt
+#####################
 
 #Konvention utgår från att version finns på 11 positionen från slutet:
 #urn:riv:crm:scheduling:GetSubjectOfCareSchedule:1:rivtabp21
@@ -18,40 +31,82 @@ FROM tp_admin.RivVersion;
 #
 # Not! Funkar inte på ett kontrakt, urn:riv13606:v1.1:RIV13606REQUEST_EHR_EXTRACT
 
-INSERT INTO takv2.Tjanstekontrakt(id, version, namnrymd, beskrivning, majorVersion) \
-SELECT id, version, namnrymd, beskrivning, substring(namnrymd,-11, 1) as majorVersion \
+INSERT INTO takv2.Tjanstekontrakt(id, version, namnrymd, beskrivning, majorVersion)
+SELECT id, version, namnrymd, beskrivning, substring(namnrymd,-11, 1) as majorVersion
 FROM tp_admin.Tjanstekontrakt;
 
-INSERT INTO takv2.Tjanstekomponent(id, version, hsaId, beskrivning) \
-SELECT id, version, hsaId, beskrivning \
+#####################
+# Tjanstekomponent
+#####################
+
+INSERT INTO takv2.Tjanstekomponent(id, version, hsaId, beskrivning)
+SELECT id, version, hsaId, beskrivning
 FROM tp_admin.Tjanstekomponent;
 
+#####################
+# LogiskAdress
+#####################
+
 # Namnbyte på tabell LogiskAdressat till LogiskAdress
-INSERT INTO takv2.LogiskAdress(id, version, hsaId, beskrivning) \
-SELECT id, version, hsaId, beskrivning \
+INSERT INTO takv2.LogiskAdress(id, version, hsaId, beskrivning)
+SELECT id, version, hsaId, beskrivning
 FROM tp_admin.LogiskAdressat;
+
+#####################
+# AnropsAdress
+#####################
+
+# Identifiera dubletter där en och samma producent exponerar samma tjänst med olika riv profiler
+# SELECT la.id
+# FROM tp_admin.LogiskAdress la, tp_admin.LogiskAdress la2
+# where la.tjansteproducent_id = la2.tjansteproducent_id
+# AND la.rivVersion_id <> la2.rivVersion_id;
 
 #Problem om det finns tjänsteproducenter som exponerar samma adress i kombination med rivta profil
 #Droppa constraint tills det är uppdaerat i TAK
-ALTER TABLE `takv2`.`AnropsAdress` \
-DROP INDEX `UC_TJANSTEKOMPONENT_ADRESS` ;
+#ALTER TABLE `takv2`.`AnropsAdress` \
+#DROP INDEX `UC_TJANSTEKOMPONENT_ADRESS` ;
 
-# Konventionen är att Anropsadress id är samma som LogiskAdress (Vagval) id för att senare koppla Anropsadress
-# till rätt Vagval.
-INSERT INTO takv2.AnropsAdress(id, version, adress, rivTaProfil_id, tjanstekomponent_id) \
-SELECT la.id, la.version, tk.adress, la.rivVersion_id as rivTaProfil_id, tk.id as tjanstekomponent_id \
-FROM tp_admin.LogiskAdress la, tp_admin.Tjanstekomponent tk \
+INSERT INTO takv2.AnropsAdress(id, version, adress, rivTaProfil_id, tjanstekomponent_id)
+SELECT DISTINCT tk.id, 0, tk.adress, la.rivVersion_id as rivTaProfil_id, tk.id as tjanstekomponent_id
+FROM tp_admin.LogiskAdress la, tp_admin.Tjanstekomponent tk
 where la.tjansteproducent_id = tk.id;
 
-INSERT INTO takv2.Anropsbehorighet(id, version, fromTidpunkt, tomTidpunkt, integrationsavtal, logiskAdress_id, tjanstekonsument_id, tjanstekontrakt_id) \
-SELECT ab.id, ab.version, ab.fromTidpunkt, ab.tomTidpunkt, ab.integrationsavtal, ab.logiskAdressat_id as logiskAdress_id, ab.tjanstekonsument_id, ab.tjanstekontrakt_id \
-FROM tp_admin.Anropsbehorighet ab \
+#######################
+# Anropsbehorighet
+#######################
+
+INSERT INTO takv2.Anropsbehorighet(id, version, fromTidpunkt, tomTidpunkt, integrationsavtal, logiskAdress_id, tjanstekonsument_id, tjanstekontrakt_id)
+SELECT ab.id, ab.version, ab.fromTidpunkt, ab.tomTidpunkt, ab.integrationsavtal, ab.logiskAdressat_id as logiskAdress_id, ab.tjanstekonsument_id, ab.tjanstekontrakt_id
+FROM tp_admin.Anropsbehorighet ab
 where ab.tjanstekonsument_id;
 
+#######################
+# Vagval
+#######################
+
 # Namnbyte på tabell LogiskAdress till Vagval
-# Konventionen är att Anropsadress_id är samma som logiskAdress_id som användes för att skapa AnropsAdress
-INSERT INTO takv2.Vagval(id, version, fromTidpunkt, tomTidpunkt, logiskAdress_id, tjanstekontrakt_id, anropsAdress_id) \
-SELECT la.id, la.version, la.fromTidpunkt, la.tomTidpunkt, la.logiskAdressat_id as logiskAdress_id, \
-	la.tjanstekontrakt_id, la.id as anropsAdress_id \
-FROM tp_admin.LogiskAdress la, tp_admin.Tjanstekomponent tk \
-where la.tjansteproducent_id = tk.id;
+INSERT INTO takv2.Vagval(version,fromTidpunkt, tomTidpunkt, logiskAdress_id, tjanstekontrakt_id, anropsAdress_id)
+SELECT 0, vagval.fromTidpunkt, vagval.tomTidpunkt, vagval.logiskAdressat_id as logiskAdress_id,
+vagval.tjanstekontrakt_id, aa.id as anropsAdress_id
+FROM tp_admin.Tjanstekomponent tk
+join tp_admin.LogiskAdress vagval
+  ON vagval.tjansteproducent_id = tk.id
+join takv2.AnropsAdress aa
+  ON tk.adress = aa.adress and aa.rivTaProfil_id = vagval.rivVersion_id;
+
+#######################
+# Filter
+#######################
+
+INSERT INTO takv2.Filter(id, version, servicedomain, anropsbehorighet_id)
+SELECT id, version, servicedomain, anropsbehorighet_id
+FROM tp_admin.Filter;
+
+#######################
+# FilterCategory
+#######################
+
+INSERT INTO takv2.Filtercategorization(id, version, category, filter_id)
+SELECT id, version, category, filter_id
+FROM tp_admin.Filtercategorization;
--- resources/sql/migrera_till_142/3-consistency-check.sql
+++ resources/sql/migrera_till_142/3-consistency-check.sql
@@ -1,11 +1,9 @@
 # Scriptet kontrollerar att varje tabell populerats med korrekt data efter
 # migrering till datamodellen för TAK 1.4.2.
-# Eventuella skillnader rapporteras av scriptet.
-#
-# 1: I de fall exakt samma data kopieras över, säkerställ att id är samma
-# 2: I de fall ny data skapats, säkerställ att korrekta id'n skapats från befintlig data
 
+#####################
 # Anvandare
+#####################
 
 SELECT 'Anvandare', id, anvandarnamn
 FROM
@@ -20,8 +18,9 @@ GROUP BY id, anvandarnamn
 HAVING COUNT(*) = 1
 ORDER BY id;
 
-
+#####################
 # Tjanstekontrakt
+#####################
 SELECT 'Tjanstekontrakt', id, namnrymd
 FROM
  (
@@ -35,7 +34,9 @@ GROUP BY id, namnrymd
 HAVING COUNT(*) = 1
 ORDER BY id;
 
+#####################
 # RivTaProfil
+#####################
 SELECT 'RivTaProfil', id, namn
 FROM
  (
@@ -49,7 +50,9 @@ GROUP BY id, namn
 HAVING COUNT(*) = 1
 ORDER BY id;
 
+#####################
 # Tjanstekomponent
+#####################
 SELECT 'Tjanstekomponent', id, hsaId
 FROM
  (
@@ -63,7 +66,9 @@ GROUP BY id, hsaId
 HAVING COUNT(*) = 1
 ORDER BY id;
 
+#####################
 # LogiskAdress
+#####################
 SELECT 'LogiskAdress', id, hsaId
 FROM
  (
@@ -77,36 +82,41 @@ GROUP BY id, hsaId
 HAVING COUNT(*) = 1
 ORDER BY id;
 
-# AnropsAdress
-SELECT 'AnropsAdress för Vagval', id, adress, rivTaProfil_id
+#####################
+# Filter
+#####################
+SELECT 'Filter', id, anropsbehorighet_id
 FROM
- (
-   SELECT la.id, tk.adress, la.rivVersion_id as rivTaProfil_id
-	  FROM tp_admin.LogiskAdress la, tp_admin.Tjanstekomponent tk
-	  where la.tjansteproducent_id = tk.id
-   UNION ALL
-   SELECT takv2.id, takv2.adress, takv2.rivTaProfil_id
-   FROM takv2.AnropsAdress takv2
+(
+  SELECT tak.id, tak.anropsbehorighet_id
+  FROM tp_admin.Filter tak
+  UNION ALL
+  SELECT takv2.id, takv2.anropsbehorighet_id
+  FROM takv2.Filter takv2
 )  t
-GROUP BY id, adress
+GROUP BY id, anropsbehorighet_id
 HAVING COUNT(*) = 1
 ORDER BY id;
 
-SELECT 'Korrekt RIV TA Profil', id, rivTaProfil_id
+#####################
+# Filtercategorization
+#####################
+SELECT 'Filtercategorization', id, filter_id
 FROM
- (
-   SELECT la.id, la.rivVersion_id as rivTaProfil_id
-	  FROM tp_admin.LogiskAdress la, tp_admin.Tjanstekomponent tk
-	  where la.tjansteproducent_id = tk.id
-   UNION ALL
-   SELECT takv2.id, takv2.rivTaProfil_id
-   FROM takv2.AnropsAdress takv2
+(
+  SELECT tak.id, tak.filter_id
+  FROM tp_admin.Filtercategorization tak
+  UNION ALL
+  SELECT takv2.id, takv2.filter_id
+  FROM takv2.Filtercategorization takv2
 )  t
-GROUP BY id, rivTaProfil_id
+GROUP BY id, filter_id
 HAVING COUNT(*) = 1
 ORDER BY id;
 
+#####################
 # Anropsbehorighet
+#####################
 SELECT 'Anropsbehorighet konsument', id, tjanstekonsument_id
 FROM
  (
@@ -133,29 +143,42 @@ GROUP BY id, logiskAdress_id
 HAVING COUNT(*) = 1
 ORDER BY id;
 
+#####################
 # Vagval
-SELECT 'Vagval korrekt logisk adress', id, logiskAdress_id
+# För varje vägval (logisk adress, tjanstekontrakt, rivta profil) skall det finnas en
+# matchande adress i Vagval
+#####################
+SELECT 'Vagval, logisk adress, tjänstekontrakt, adress', logiskAdress_id, tjanstekontrakt_id, rivTaProfil_id,adress
 FROM
- (
-   SELECT tak.id, tak.logiskAdressat_id as logiskAdress_id
-   FROM tp_admin.LogiskAdress tak
-   UNION ALL
-   SELECT takv2.id, takv2.logiskAdress_id
-   FROM takv2.Vagval takv2
+(
+  SELECT la.logiskAdressat_id as logiskAdress_id, la.tjanstekontrakt_id, la.rivVersion_id as rivTaProfil_id, tk.adress
+  FROM tp_admin.LogiskAdress la, tp_admin.Tjanstekomponent tk
+  WHERE la.tjansteproducent_id = tk.id
+  UNION ALL
+  SELECT vagval.logiskAdress_id, vagval.tjanstekontrakt_id, aa.rivTaProfil_id, aa.adress
+  FROM takv2.Vagval vagval, takv2.AnropsAdress aa
+  WHERE vagval.anropsAdress_id = aa.id
 )  t
-GROUP BY id, logiskAdress_id
+GROUP BY logiskAdress_id, tjanstekontrakt_id, rivTaProfil_id, adress
 HAVING COUNT(*) = 1
-ORDER BY id;
+ORDER BY logiskAdress_id;
 
-SELECT 'Vagval korrekt tjänstekontrakt', id, tjanstekontrakt_id
+#####################
+# AnropsAdress
+# Varje tjänsteproducent måste ha en anropsadress med
+# korrekt rivta profil. I tidigare databasen låg denna
+# informationen redundant, därav distinct.
+#####################
+SELECT 'AnropsAdress och RIV TA profil för Vagval', id, adress, rivTaProfil_id
 FROM
- (
-   SELECT tak.id, tak.tjanstekontrakt_id
-   FROM tp_admin.LogiskAdress tak
-   UNION ALL
-   SELECT takv2.id, takv2.tjanstekontrakt_id
-   FROM takv2.Vagval takv2
+(
+  SELECT DISTINCT tk.id, tk.adress, la.rivVersion_id as rivTaProfil_id
+  FROM tp_admin.LogiskAdress la, tp_admin.Tjanstekomponent tk
+  where la.tjansteproducent_id = tk.id
+  UNION ALL
+  SELECT takv2.tjanstekomponent_id, takv2.adress, takv2.rivTaProfil_id
+  FROM takv2.AnropsAdress takv2
 )  t
-GROUP BY id, tjanstekontrakt_id
+GROUP BY id, adress, rivTaProfil_id
 HAVING COUNT(*) = 1
 ORDER BY id;
