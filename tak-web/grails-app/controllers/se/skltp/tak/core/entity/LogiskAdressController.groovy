785d59abb4211adc3ade7be3ef11531209d427f8
Martin Flower <martin.flower@callistaenterprise.se>
2015-03-26 14:24:44 +0100

    SKLTP-531 bulk upload logiska adress

--- tak-web/grails-app/controllers/se/skltp/tak/core/entity/LogiskAdressController.groovy
+++ tak-web/grails-app/controllers/se/skltp/tak/core/entity/LogiskAdressController.groovy
@@ -22,6 +22,8 @@ package se.skltp.tak.core.entity
 
 import org.grails.plugin.filterpane.FilterPaneUtils
 
+import se.skltp.tak.web.command.LogiskaAdresserBulk;
+
 class LogiskAdressController {
 
     def scaffold = LogiskAdress
@@ -35,4 +37,102 @@ class LogiskAdressController {
 						filterParams: FilterPaneUtils.extractFilterParams(params),
 						params:params ] )
 	}
+    
+    def bulkcreate() {
+        log.info 'bulkcreate'
+        render (view:'bulkcreate')
+    }
+    
+    def bulkvalidate(LogiskaAdresserBulk lb) {
+        if (!lb.logiskaAdresserBulk) {
+        log.debug("bulkvalidate - no input parameter - redirecting to bulkcreate (probably user navigation error)")
+            redirect (action:'bulkcreate')
+        }
+
+        def lines = lb.logiskaAdresserBulk.split("[\\r\\n]+")
+        /*
+        SE162321000255-O16560,  (Flexlab) Barnmorskemottagning Skurup
+        SE162321000255-O16561,  (Flexlab) Barnmorskemottagning Ystad
+        SE2321000016-12ZX, Solna Ungdomsmottagning
+        .. ..
+        */
+        
+        lines.each {
+            if (it) {
+                def line = it.split(",",2)
+                if (line.size() != 2) {
+                    lb.rejectedLines << it + " [${message(code:'missing.comma')}]"
+                } else {
+                    line[0] = line[0].trim().toUpperCase() // hsa id uppercase
+                    line[1] = line[1].trim()
+                    
+                    if (!line[0]) {
+                        lb.rejectedLines << it + " [${message(code:'missing.hsaid')}]"
+                    } else if (!line[1]) {
+                        lb.rejectedLines << it + " [${message(code:'missing.description')}]"
+                    } else {
+                        def existingHsaIds = LogiskAdress.findAllByHsaIdIlike(line[0])
+                        if (existingHsaIds != null && !existingHsaIds.isEmpty()) {
+                            lb.rejectedLines << it + " [${message(code:'hsaid.alreadyexists')}] ${existingHsaIds.get(0).beskrivning}]"
+                        } else if (lb.acceptedLines.containsKey(line[0])) {
+                            lb.rejectedLines << it + " [${message(code:'hsaid.existsmorethanonceinimport')}]"
+                        } else {
+                            lb.acceptedLines.put(line[0], line[1])
+                        }
+                    }
+                }
+            }
+        }
+
+        if (lb.acceptedLines.isEmpty()) {
+            lb.rejectedLines.each {
+                log.info(it)
+            }
+            flash.message = message(code:'novalidimportlines') 
+            render (view:'bulkcreate', model:[logiskaAdresserBulk:lb.logiskaAdresserBulk])
+        } else {
+            // store LogiskaAdresserBulk in flash scope for next step (bulksave)
+            flash.lb = lb
+            flash.message = message(code:'clickcreatenewlogicaladdresses')
+            render (view:'bulkconfirm', model:[logiskaAdresserBulk:lb])
+        }
+    }
+        
+    def bulksave() {
+        log.info 'bulksave'
+        LogiskaAdresserBulk lb = flash.lb
+        if (lb == null || lb.acceptedLines.empty) {
+            log.debug("bulksave - no command in flash scope - redirecting to bulkcreate (probably user navigation error)")
+            redirect(action: 'bulkcreate')
+        } else {
+            
+            int countSuccess = 0
+            int countFailed  = 0
+            int countExist   = 0
+            
+            lb.acceptedLines.each  { line ->
+                def existingHsaId = LogiskAdress.findAllByHsaId(line.key)
+                if (existingHsaId != null && !existingHsaId.isEmpty()) {
+                    countExist++
+                    log.warn("Not creating duplicate LogiskAdress HSA id ${line.key}")
+                } else {
+                    LogiskAdress l = new LogiskAdress()
+                    l.hsaId = line.key
+                    l.beskrivning = line.value
+                    def result = l.save()
+                    if (result == null) {
+                        countFailed++
+                        log.error("Failed to save LogiskAdress hsa id ${line.key}")
+                    } else {
+                        countSuccess++
+                        log.info("Saved LogiskAdress ${l.id} (${l.hsaId})")
+                    }
+                }
+            }
+            
+            flash.message = message(code:'createdlogicaladdresses',args:[countSuccess,countExist,countFailed])
+            redirect(action: 'list')
+            return
+        }
+    }
 }
--- tak-web/grails-app/views/logiskAdress/list.gsp
+++ tak-web/grails-app/views/logiskAdress/list.gsp
@@ -34,8 +34,15 @@
 		<a href="#list-logiskAdress" class="skip" tabindex="-1"><g:message code="default.link.skip.label" default="Skip to content&hellip;"/></a>
 		<div class="nav" role="navigation">
 			<ul>
-				<li><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></li>
-				<li><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></li>
+				<li>
+                   <a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a>
+                </li>
+				<li>
+                   <g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link>
+                </li>
+                <li>
+                   <g:link class="create" action="bulkcreate"><g:message code="bulkcreate.label" /></g:link>
+                </li>
 			</ul>
 		</div>
 		<div id="list-logiskAdress" class="content scaffold-list" role="main">
