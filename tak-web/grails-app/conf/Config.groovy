e804c5b67708c59527551adea4c0b9c5e017c25e
hanswiklund <hans.wiklund@kentor.se>
2016-06-21 12:45:00 +0200

    SKLTP-263

--- tak-web/grails-app/conf/Config.groovy
+++ tak-web/grails-app/conf/Config.groovy
@@ -23,7 +23,6 @@
 
  grails.config.locations = []
 
-grails.config.locations << "file:/var/spool/tomcat/tak/etc/tak-web-config.properties"
 if(System.getenv('TAK_HOME')) {
 	println "System variable TAK_HOME was found! External property-files will be configured if found."
 	grails.config.locations << "file:${System.getenv('TAK_HOME')}/${appName}-config.properties"
