f414da73bf7fa0a66108aabb6fbc320d6f218b12
hanswiklund <hans.wiklund@kentor.se>
2017-05-02 14:28:18 +0200

    changed from locktb to Locktb

--- resources/sql/tp-admin-DDL.sql
+++ resources/sql/tp-admin-DDL.sql
@@ -205,11 +205,11 @@ CREATE INDEX anropsbehorighet_distinct_idx ON Anropsbehorighet (
   `tjanstekontrakt_id`,
   `deleted`);
   
-CREATE TABLE locktb (
+CREATE TABLE Locktb (
   tabell varchar(100) NOT NULL,
   locked int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`tabell`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO locktb(`tabell`,`locked`)
+INSERT INTO Locktb(`tabell`,`locked`)
 VALUES('PubVersion',0);
