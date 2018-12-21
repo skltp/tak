1f1858bc14db7fce581b4467c9094cb4150b4f5a
mtuliakova <mariia.tuliakova@soprasteria.com>
2018-12-21 11:37:44 +0100

    bugfix

--- resources/sql/v2.3.4/skapa-nya-properties.sql
+++ resources/sql/v2.3.4/skapa-nya-properties.sql
@@ -1,4 +1,4 @@
 INSERT INTO `TAKSettings` (`id`, `settingName`, `settingValue` ,`version`) VALUES
 (7, 'mail.alerter.ny.tjanstekontrakt.toAddress', 'ny_kontrakt_toAddress@server.com', 0),
 (9, 'mail.alerter.ny.tjanstekontrakt.subject', 'Nytt kontrakt är skapad från json beställning', 0),
-(10, 'mail.alerter.ny.tjanstekontrakt.text', 'Ett nytt kontrakt med namnet ${contractName} är skapad och aktiveras ${date}', 0);
+(10, 'mail.alerter.ny.tjanstekontrakt.text', 'Ett nytt kontrakt med namnet ${contractName} är skapat och aktiveras ${date}', 0);
