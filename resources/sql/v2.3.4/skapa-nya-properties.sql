4bbbfdb3988e292fb1b11b08c80416f47d599013
mtuliakova <mariia.tuliakova@soprasteria.com>
2018-12-21 11:27:27 +0100

    ny setting för skicka mail

---
+++ resources/sql/v2.3.4/skapa-nya-properties.sql
@@ -0,0 +1,4 @@
+INSERT INTO `TAKSettings` (`id`, `settingName`, `settingValue` ,`version`) VALUES
+(7, 'mail.alerter.ny.tjanstekontrakt.toAddress', 'ny_kontrakt_toAddress@server.com', 0),
+(9, 'mail.alerter.ny.tjanstekontrakt.subject', 'Nytt kontrakt är skapad från json beställning', 0),
+(10, 'mail.alerter.ny.tjanstekontrakt.text', 'Ett nytt kontrakt med namnet ${contractName} är skapad och aktiveras ${date}', 0);
