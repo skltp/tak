9eb5a89853e312d651d2302d3ee6a1bbadaf8564
Jonas Mattsson <jonas.mattsson@nordicmedtest.se>
2022-03-29 16:30:49 +0200

    using new values an attributes suggested by tomcat instead of deprecated ditos

---
+++ tak-services/src/main/java/se/skltp/tak/response/GetStatusResponse.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2013 Center for eHalsa i samverkan (CeHis).
+ * 							<http://cehis.se/>
+ *
+ * This file is part of SKLTP.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/**
+ * 
+ */
+package se.skltp.tak.response;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.security.SecureRandom;
+import java.util.*;
+
+/**
+ * @author muqkha
+ *
+ */
+@XmlRootElement
+public class GetStatusResponse {
+	
+	private String message;
+
+	private Map<String, String> servicesList = new HashMap<String, String>();
+
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	@XmlElementWrapper(name="appinfo")
+	public Map<String, String> getServicesList() {
+		return servicesList;
+	}	
+	public void setAppInfoList(String app, String info) {
+		this.servicesList.put(app, info);
+	}
+}
---
+++ tak-services/src/main/java/se/skltp/tak/services/GetApplicationStatus.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2013 Center for eHalsa i samverkan (CeHis).
+ * 							<http://cehis.se/>
+ *
+ * This file is part of SKLTP.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package se.skltp.tak.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.skltp.tak.response.GetStatusResponse;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import org.apache.catalina.util.ServerInfo;
+
+import java.io.IOException;
+import java.util.*;
+
+@Path("/")
+public class GetApplicationStatus {
+	private static final Logger log = LoggerFactory.getLogger(GetApplicationStatus.class);
+
+	public GetApplicationStatus() {}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/status")
+	public GetStatusResponse getApplicationStatus() {
+		log.debug("Received status request");
+		ServerInfo si = new ServerInfo();
+
+		GetStatusResponse statusResp = new GetStatusResponse();
+		String java_version = System.getProperty("java.version");
+
+		final Properties properties = new Properties();
+		try {
+			log.debug("loading project.properties");
+			properties.load(getClass().getClassLoader().getResourceAsStream("project.properties"));
+		} catch (IOException e) {
+			log.warn(e.toString());
+			e.printStackTrace();
+		}
+
+		for (Object key : properties.keySet()) {
+			statusResp.setAppInfoList((String) key, properties.getProperty((String) key));
+		}
+
+		statusResp.setAppInfoList("servletengine.info", si.getServerInfo());
+		statusResp.setAppInfoList("java.vendor", System.getProperty("java.vendor"));
+		statusResp.setAppInfoList("java.version", System.getProperty("java.version"));
+		statusResp.setAppInfoList("os.arch", System.getProperty("os.arch"));
+		statusResp.setAppInfoList("os.name", System.getProperty("os.name"));
+		statusResp.setAppInfoList("os.version", System.getProperty("os.version"));
+		statusResp.setAppInfoList("user.name", System.getProperty("user.name"));
+
+		return statusResp;
+	}
+
+}
