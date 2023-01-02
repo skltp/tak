b15468d687dc4d388c7d79708ee86d3ad4dcc9f1
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2023-01-02 16:53:11 +0100

    Fixed some broken tests

--- tak-web/src/test/java/se/skltp/tak/web/controller/CrudControllerTests.java
+++ tak-web/src/test/java/se/skltp/tak/web/controller/CrudControllerTests.java
@@ -39,6 +39,8 @@ public class CrudControllerTests {
     @MockBean VagvalService vagvalService;
     @MockBean AnropsAdressService anropsAdressService;
     @MockBean AnropsBehorighetService anropsBehorighetService;
+    @MockBean FilterService filterService;
+    @MockBean FilterCategorizationService filterCategorizationService;
 
     MockedStatic<SecurityUtils> securityUtilsMock;
     Subject mockSubject;
