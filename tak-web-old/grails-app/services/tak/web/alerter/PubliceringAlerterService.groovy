e14fe8c805427ea157dacc5006f5437e3cfb2f5b
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2023-02-21 08:03:35 +0100

    Deleted old grails web

--- tak-web-old/grails-app/services/tak/web/alerter/PubliceringAlerterService.groovy
+++
@@ -1,9 +0,0 @@
-package tak.web.alerter
-
-import se.skltp.tak.core.entity.PubVersion
-
-interface PubliceringAlerterService {
-    void alertOnPublicering(PubVersion pubVersionInstance);
-    void alertOnRollback(PubVersion pubVersionInstance)
-
-}
