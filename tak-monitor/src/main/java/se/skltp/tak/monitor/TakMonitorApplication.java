484f04a0e110f76f1da9248d8649c6ad94052377
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2023-06-26 16:32:28 +0200

    Simplified CoreV1Api injection

--- tak-monitor/src/main/java/se/skltp/tak/monitor/TakMonitorApplication.java
+++ tak-monitor/src/main/java/se/skltp/tak/monitor/TakMonitorApplication.java
@@ -1,11 +1,17 @@
 package se.skltp.tak.monitor;
 
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.util.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableScheduling
@@ -15,5 +21,10 @@ public class TakMonitorApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(TakMonitorApplication.class, args);
+  }
+
+  @Bean
+  CoreV1Api coreV1Api() throws IOException {
+    return new CoreV1Api(Config.defaultClient());
   }
 }
