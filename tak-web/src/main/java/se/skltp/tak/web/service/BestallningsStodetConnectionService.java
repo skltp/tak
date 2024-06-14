ade33cea4ac428f7d10e6cf09d1496b40bb71ad0
Mariia Tuliakova <mariia.tuliakova@nordicmedtest.se>
2024-06-14 15:22:11 +0200

    mer loggning

--- tak-web/src/main/java/se/skltp/tak/web/service/BestallningsStodetConnectionService.java
+++ tak-web/src/main/java/se/skltp/tak/web/service/BestallningsStodetConnectionService.java
@@ -1,5 +1,7 @@
 package se.skltp.tak.web.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import java.util.Set;
 
 @Service
 public class BestallningsStodetConnectionService {
+    private static final Logger log = LoggerFactory.getLogger(BestallningsStodetConnectionService.class);
 
     @Autowired
     ConfigurationService configurationService;
@@ -55,9 +58,11 @@ public class BestallningsStodetConnectionService {
     }
 
     public String getBestallning(long bestallningsNummer) throws Exception {
+        log.info("Hämtar beställning nummer {}", bestallningsNummer);
         StringBuilder jsonBestallning = new StringBuilder();
 
         URL url = new URL(configurationService.getBestallningUrl() + bestallningsNummer);
+        log.info("Url: {}", url);
 
         HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
         con.setSSLSocketFactory(prepareSSLContext().getSocketFactory());
