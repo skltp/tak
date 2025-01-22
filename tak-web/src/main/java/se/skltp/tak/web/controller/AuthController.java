0f96fcd86e8e9629d92875f911a7396abdc60a82
Jonas Mattsson <jonas.mattsson@nordicmedtest.se>
2025-01-22 14:23:10 +0100

    fixed null-bug

--- tak-web/src/main/java/se/skltp/tak/web/controller/AuthController.java
+++ tak-web/src/main/java/se/skltp/tak/web/controller/AuthController.java
@@ -5,12 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import static java.lang.Boolean.TRUE;
+
 @Controller
 public class AuthController {
 
     @GetMapping("/auth/login")
     public String login(Model model, @RequestParam(required = false) Boolean error) {
-        if (error.equals(true)) {
+        if (TRUE.equals(error)) {
             model.addAttribute("message", "Ogiltigt användarnamn och/eller lösenord");
         }
 
