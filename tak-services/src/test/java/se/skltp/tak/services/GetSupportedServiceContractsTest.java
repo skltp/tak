252e3d06a094418d1f8f958deec0850efa5c5eb9
Henrik A <henrik.augustsson@inera.se>
2026-04-16 18:52:38 +0200

    - log4j-api moved to top level for cross-project sync. - every outdated license reference in file headers have been removed and cleaned out. - polished the license files at the root. This might be used as basis for new more compact file-head notices.

--- tak-services/src/test/java/se/skltp/tak/services/GetSupportedServiceContractsTest.java
+++ tak-services/src/test/java/se/skltp/tak/services/GetSupportedServiceContractsTest.java
@@ -1,23 +1,3 @@
-/**
- * Copyright (c) 2013 Center for eHalsa i samverkan (CeHis).
- * 							<http://cehis.se/>
- *
- * This file is part of SKLTP.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
 package se.skltp.tak.services;
 
 import org.junit.jupiter.api.Test;
@@ -29,10 +9,7 @@ import se.rivta.itintegration.registry.getsupportedservicecontractsresponder.v1.
 import se.rivta.itintegration.registry.getsupportedservicecontractsresponder.v1.GetSupportedServiceContractsType;
 
 /**
- * Test of the get supported service contracts
- * implementation
- *
- * @author Marcus Krantz [marcus.krantz@callistaenterprise.se]
+ * Test of the "get supported service contracts" implementation.
  */
 @SpringBootTest(classes = {TakServicesApplication.class})
 class GetSupportedServiceContractsTest extends AbstractServiceTest {
--- tak-services/src/test/java/se/skltp/tak/services/TestConsumer.java
+++ tak-services/src/test/java/se/skltp/tak/services/TestConsumer.java
@@ -1,23 +1,3 @@
-/**
- * Copyright (c) 2013 Center for eHalsa i samverkan (CeHis).
- * 							<http://cehis.se/>
- *
- * This file is part of SKLTP.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
 package se.skltp.tak.services;
 
 import java.net.MalformedURLException;
@@ -34,9 +14,6 @@ import se.skltp.tak.vagvalsinfo.wsdl.v2.HamtaAllaVirtualiseringarResponseType;
 import se.skltp.tak.vagvalsinfo.wsdl.v2.SokVagvalsInfoInterface;
 import se.skltp.tak.vagvalsinfo.wsdl.v2.SokVagvalsServiceSoap11LitDocService;
 
-/**
- * Created by magnus on 25/10/14.
- */
 public class TestConsumer {
     private static final Logger logger = LoggerFactory.getLogger(TestConsumer.class);
     private final SokVagvalsInfoInterface port;
