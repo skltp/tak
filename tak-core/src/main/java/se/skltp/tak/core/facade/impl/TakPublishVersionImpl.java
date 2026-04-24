554fa1125edfddfd4462623e2728bab49846bfa5
Henrik A <henrik.augustsson@inera.se>
2026-04-24 16:51:42 +0200

    - new license headers

--- tak-core/src/main/java/se/skltp/tak/core/facade/impl/TakPublishVersionImpl.java
+++ tak-core/src/main/java/se/skltp/tak/core/facade/impl/TakPublishVersionImpl.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2014-2026 Inera.
+ * Copyright owner URL: https://www.inera.se/
+ * SKLTP overview page: https://inera.atlassian.net/wiki/spaces/SKLTP/overview
+ * This library is free software under the GNU Lesser General Public License v2.1 or later.
+ * Please refer to the full license files at the project root.
+ */
 package se.skltp.tak.core.facade.impl;
 
 import java.io.ByteArrayOutputStream;
